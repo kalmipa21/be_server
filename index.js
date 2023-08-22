457624f9b23fd12661a9cf9d8e03151ebe41dce7
kalmipa21 <aripsapu@gmail.com>
2023-08-22 09:19:53 +0700

    Handle CORS for any port and proxy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import r_address from "./src/routers/r_address.js";
 
 //Import Router
 const app = express();
-app.use(cors({ origin: `http://localhost:${PORT}` }));
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
